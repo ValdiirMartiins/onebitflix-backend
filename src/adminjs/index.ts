07589361ca6b1bd685769f74dee338ee0912839c
Valdir Martins <valdiirmartins2502@gmail.com>
2024-02-10 00:32:18 -0300

    onebitflix-backend

---
+++ src/adminjs/index.ts
@@ -0,0 +1,30 @@
+import AdminJs from "adminjs";
+import AdminJsExpress from "@adminjs/express";
+import AdminJsSequelize from "@adminjs/sequelize";
+import { sequelize } from "../database";
+import { adminJsResources } from "./resources";
+import { locale } from "./locale";
+import { dashboardOptions } from "./dashboard";
+import { brandingOptions } from "./branding";
+import { authenticationOptions } from "./autantication";
+
+AdminJs.registerAdapter(AdminJsSequelize);
+
+export const adminJs = new AdminJs({
+  databases: [sequelize],
+  rootPath: "/admin",
+  resources: adminJsResources,
+  branding: brandingOptions,
+  locale: locale,
+  dashboard: dashboardOptions,
+});
+
+export const adminJsRouter = AdminJsExpress.buildAuthenticatedRouter(
+  adminJs,
+  authenticationOptions,
+  null,
+  {
+    resave: false,
+    saveUninitialized: false,
+  }
+);
